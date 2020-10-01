e744deb5734c3e800485f8e0102e971007068ead
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-10-01 18:17:31 -0300

    frontend

--- src/Graph.js
+++ src/Graph.js
@@ -1,283 +1,147 @@
 // import graph from './assets/graph.json';
-const graph = {
-	empresas_habilitadas_regular: [
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "08/07/2016",
-			"prefixo": "12-0140-00",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "CALDAS NOVAS",
-			"uf_destino": "GO",
-			"extensao_secao": "318.10",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "08/07/2016",
-			"prefixo": "12-0140-00",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "VIANOPOLIS",
-			"uf_destino": "GO",
-			"extensao_secao": "158.50",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "08/07/2016",
-			"prefixo": "12-0140-00",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "VIANOPOLIS",
-			"uf_destino": "GO",
-			"extensao_secao": "168.60",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "08/07/2016",
-			"prefixo": "12-0140-00",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "ORIZONA",
-			"uf_destino": "GO",
-			"extensao_secao": "210.60",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "08/07/2016",
-			"prefixo": "12-0140-00",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "PIRES DO RIO",
-			"uf_destino": "GO",
-			"extensao_secao": "243.60",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "08/07/2016",
-			"prefixo": "12-0140-00",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "PIRES DO RIO",
-			"uf_destino": "GO",
-			"extensao_secao": "248.51",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "08/07/2016",
-			"prefixo": "12-0140-00",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "PIRES DO RIO",
-			"uf_destino": "GO",
-			"extensao_secao": "254.87",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "06/11/2018",
-			"prefixo": "12-0140-61",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "CALDAS NOVAS",
-			"uf_destino": "GO",
-			"extensao_secao": "318.10",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "06/11/2018",
-			"prefixo": "12-0140-61",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "VIANOPOLIS",
-			"uf_destino": "GO",
-			"extensao_secao": "158.50",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "06/11/2018",
-			"prefixo": "12-0140-61",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "VIANOPOLIS",
-			"uf_destino": "GO",
-			"extensao_secao": "168.60",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "06/11/2018",
-			"prefixo": "12-0140-61",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "ORIZONA",
-			"uf_destino": "GO",
-			"extensao_secao": "210.60",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "06/11/2018",
-			"prefixo": "12-0140-61",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "PIRES DO RIO",
-			"uf_destino": "GO",
-			"extensao_secao": "243.60",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "06/11/2018",
-			"prefixo": "12-0140-61",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "PIRES DO RIO",
-			"uf_destino": "GO",
-			"extensao_secao": "248.51",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "06/11/2018",
-			"prefixo": "12-0140-61",
-			"descricao_linha": "BRASILIA(DF) - CALDAS NOVAS(GO)",
-			"municipio_origem": "BRASILIA",
-			"uf_origem": "DF",
-			"municipio_destino": "PIRES DO RIO",
-			"uf_destino": "GO",
-			"extensao_secao": "254.87",
-			"outorga_secao": "AUTORIZAÇÃO "
-		},
-		{
-			"cnpj": "04.192.453/0001-18",
-			"razao_social": "ALFA LUZ VIACAO TRANSPORTE LTDA",
-			"numero_tar": "145",
-			"numero_lop": "17",
-			"data_autorizacao": "15/05/2017",
-			"prefixo": "12-0274-00",
-			"descricao_linha": "CALDAS NOVAS(GO) - ARAGUARI(MG)",
-			"municipio_origem": "CALDAS NOVAS",
-			"uf_origem": "GO",
-			"municipio_destino": "ARAGUARI",
-			"uf_destino": "MG",
-			"extensao_secao": "139.80",
-			"outorga_secao": "AUTORIZAÇÃO "
-		}
-	]
-};
+const graph = [
+	{
+		nome: 'ALFA LUZ VIACAO TRANSPORTE LTDA',
+		origem: 'BRASILIA',
+		destino: 'CALDAS NOVAS',
+		extensao: 100
+	},
+	{
+		nome: 'ALFA LUZ VIACAO TRANSPORTE LTDA',
+		origem: 'CALDAS NOVAS',
+		destino: 'VIANÓPOLIS',
+		extensao: 200
+	}
+];
 
 export default class Graph {
 	edges = {};
 	nodes = [];
-
-	constructor() {
-		graph.empresas_habilitadas_regular.forEach((linha) => {
-			if (this.nodes.findIndex(node => node.municipio_origem === linha.municipio_origem) === -1) {
-				this.addNode(linha);
-			}
-		});
-		this.nodes.forEach((node) => {
-			const destinos = graph.empresas_habilitadas_regular.filter((linha) => linha.municipio_origem === node.municipio_destino);
-			destinos.forEach((destino) => {
-				this.addDirectedEdge(node, destino);
-			});
-		});
-
-		this.display();
-	}
 
 	addNode(node) {
 		this.nodes.push(node);
 		this.edges[node] = [];
 	}
 
-	addEdge(node1, node2, weight = 1) {
-		this.edges[node1].push({ node: node2, weight: weight });
-		this.edges[node2].push({ node: node1, weight: weight });
-	}
-
-	addDirectedEdge(node1, node2, weight = 1) {
-		console.log(node1, node2);
+	addEdge(node1, node2, weight) {
+		if (!this.edges[node1]) {
+			this.edges[node1] = [];
+		}
 		this.edges[node1].push({ node: node2, weight: weight });
 	}
-
-	// addEdge(node1, node2) {
-	//   this.edges[node1].push(node2);
-	//   this.edges[node2].push(node1);
-	// }
-
-	// addDirectedEdge(node1, node2) {
-	//   this.edges[node1].push(node2);
-	// }
 
 	display() {
 		let graph = "";
 		this.nodes.forEach(node => {
-			graph += node.municipio_origem + "->" + this.edges[node].map(n => n.node.municipio_destino).join(", ") + "\n";
+			graph += node + "->" + this.edges[node].map(n => n.node).join(", ") + "\n";
 		});
 		console.log(graph);
 	}
+
+	djikstraAlgorithm(startNode) {
+		let distances = {};
+	
+		// Stores the reference to previous nodes
+		let prev = {};
+		let pq = new PriorityQueue(this.nodes.length * this.nodes.length);
+	
+		// Set distances to all nodes to be infinite except startNode
+		distances[startNode] = 0;
+		pq.enqueue(startNode, 0);
+		this.nodes.forEach(node => {
+			 if (node !== startNode) distances[node] = Infinity;
+			 prev[node] = null;
+		});
+	
+		while (!pq.isEmpty()) {
+			 let minNode = pq.dequeue();
+			 let currNode = minNode.data;
+			 this.edges[currNode].forEach(neighbor => {
+					let alt = distances[currNode] + neighbor.weight;
+					if (alt < distances[neighbor.node]) {
+						 distances[neighbor.node] = alt;
+						 prev[neighbor.node] = currNode;
+						 pq.enqueue(neighbor.node, distances[neighbor.node]);
+					}
+			 });
+		}
+		return distances;
+	}
 }
+
+class PriorityQueue {
+	constructor(maxSize) {
+		 // Set default max size if not provided
+		 if (isNaN(maxSize)) {
+				maxSize = 10;
+			}
+		 this.maxSize = maxSize;
+		 // Init an array that'll contain the queue values.
+		 this.container = [];
+	}
+	// Helper function to display all values while developing
+	display() {
+		 console.log(this.container);
+	}
+	// Checks if queue is empty
+	isEmpty() {
+		 return this.container.length === 0;
+	}
+	// checks if queue is full
+	isFull() {
+		 return this.container.length >= this.maxSize;
+	}
+	enqueue(data, priority) {
+		 // Check if Queue is full
+		 if (this.isFull()) {
+				console.log("Queue Overflow!");
+				return;
+		 }
+		 let currElem = new this.Element(data, priority);
+		 let addedFlag = false;
+		 // Since we want to add elements to end, we'll just push them.
+		 for (let i = 0; i < this.container.length; i++) {
+				if (currElem.priority < this.container[i].priority) {
+					 this.container.splice(i, 0, currElem);
+					 addedFlag = true; break;
+				}
+		 }
+		 if (!addedFlag) {
+				this.container.push(currElem);
+		 }
+	}
+	dequeue() {
+		// Check if empty
+		if (this.isEmpty()) {
+			console.log("Queue Underflow!");
+			return;
+		}
+		return this.container.pop();
+	}
+	peek() {
+		if (this.isEmpty()) {
+			console.log("Queue Underflow!");
+			return;
+		}
+		return this.container[this.container.length - 1];
+	}
+	clear() {
+		this.container = [];
+		}
+}
+// Create an inner class that we'll use to create new nodes in the queue
+// Each element has some data and a priority
+PriorityQueue.prototype.Element = class {
+	constructor(data, priority) {
+		 this.data = data;
+		 this.priority = priority;
+	}
+};
+
+let g = new Graph();
+graph.forEach(item => {
+	g.addNode(item.origem);
+	g.addEdge(item.origem, item.destino, item.extensao);
+});
+
+console.log(g.djikstraAlgorithm("BRASILIA"));
