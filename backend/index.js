3b9fd43bfc687424793e96517c7feb4443b1de41
Guilherme Siqueira <guilhersiqueira@gmail.com>
2020-10-01 21:21:27 -0300

    using dijkstra to get the shortest distance between two municipalities

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
 const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
 const Graph = require('./graph.js');
 
 const app = express();
 const port = 5000;
+
+app.use(bodyParser.json())
+app.use(cors());
+
+let graph;
 
 app.get('/', async(_, res) => {
   fs.readFile('data.json', (_, data) => {
     // process data
     const linhas = JSON.parse(data);
   
-    let graph = new Graph();
+    graph = new Graph();
     linhas.forEach(({ municipio_origem }) => {
       graph.addNode(municipio_origem);
     });
@@ -18,9 +25,16 @@ app.get('/', async(_, res) => {
     linhas.forEach((linha) => {
       graph.addEdge(linha.municipio_origem, linha.municipio_destino, linha.extensao_secao);
     });
-  
-    res.send(graph.djikstraAlgorithm('BRASILIA'));
+
+    res.send({nodes: graph.nodes, edges: graph.edges});  
   });
 });
+
+app.post('/rotas', function (req, res) {
+  const data = req.body;
+  const distances = graph.djikstraAlgorithm(data.municipio_origem);
+  res.send({distancia: distances[data.municipio_destino]});
+
+})
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
